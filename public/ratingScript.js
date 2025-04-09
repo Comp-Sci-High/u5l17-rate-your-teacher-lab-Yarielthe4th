d1373de52f31072b014fe8662e8d14e7efbbf791
Yarielthe4th <yariel.beato25@compscihigh.org>
2025-04-09 14:44:44 +0000

    done

---
+++ public/ratingScript.js
@@ -0,0 +1,22 @@
+// Add an event listener to the form that upon submits creates a new rating
+
+const { response } = require("express")
+
+// When the rating is created redirect to the ratings page
+const createForm = document.querySelector("form")
+
+createForm.addEventListener("submit", async (e)=>{
+	e.preventDefault()
+	const ratingData = new FormData(createForm)
+	const reqBody = Object.fromEntries(ratingData)
+
+	const response = await fetch("/add/rating", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(reqBody)
+	})
+    const data = await response.JSON()
+    console.log(data)
+})
---
+++ public/teacherScript.js
@@ -0,0 +1,17 @@
+// Add an event listener to the form that upon submits creates a new teacher
+// When the teacher is created redirect to the teachers page
+const createForm = document.querySelector(“form”)
+
+createForm.addEventListener(“submit”, async (e)=>{
+	e.preventDefault()
+	const bookData = new FormData(createForm)
+	const reqBody = Object.fromEntries(bookData)
+
+	const response = await fetch(“/add”, {
+		method: “POST”,
+		headers: {
+			“Content-Type”: “application/json”)
+		},
+		body: JSON.stringify(reqBody)
+	})
+})
